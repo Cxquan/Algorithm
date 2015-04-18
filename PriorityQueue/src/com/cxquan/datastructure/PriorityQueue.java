1f91e921201835140f0868e1a643cbca2587f40d
Cxquan <838612286@qq.com>
2015-04-18 16:41:09 +0800

    add maxheap

---
+++ PriorityQueue/src/com/cxquan/datastructure/PriorityQueue.java
@@ -0,0 +1,13 @@
+package com.cxquan.datastructure;
+
+
+public class PriorityQueue {
+
+	
+//	BlockingQueue<Integer> 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
