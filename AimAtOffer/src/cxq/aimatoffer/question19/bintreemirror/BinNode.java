712b08c545af6085935213b0362a40a0765a3360
Cxquan <838612286@qq.com>
2016-03-01 14:04:33 +0800

    dfdf

---
+++ AimAtOffer/src/cxq/aimatoffer/question19/bintreemirror/BinNode.java
@@ -0,0 +1,20 @@
+package cxq.aimatoffer.question19.bintreemirror;
+
+public class BinNode {
+    int value;
+    BinNode left = null;
+    BinNode right = null;
+    
+    public BinNode(int value) {
+        this.value = value;
+    }
+    
+    public static void print(BinNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.value);
+        print(root.left);
+        print(root.right);
+    }
+}
